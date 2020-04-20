c5f0c592593530cd7ebafd06c36504c5f19d5233
whiteosf <60788417+whiteosf@users.noreply.github.com>
2020-04-20 19:22:18 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,40 +24,58 @@ async def say_hello(ctx):
     await ctx.send(f"Hi {ctx.author.display_name}")
 
 @bot.command(name = "equ", help="Solves your sacrifised quadratic and linear equations, takes in three arguments: a, b, c, which are coefficients. Even if coefficient is 0, it has to be listed. As example for equation 3x-4=0 you have to send 0 3 -4 as argumants.")
-async def equation(ctx, a, b, c):
-    if a==0:
-        await ctx.send(f"{ctx.author.display_name}, equation: x = {(-c)/b}")
+async def equation(ctx, a:int, b:int, c:int):
+    if a==0 and b!=0:
+        await ctx.send( f"{ctx.author.display_name}, equation: x = {(-c)/b}")
     elif a!=0:
         discr= b**2 - 4*a*c
-        sqr = square_root_simplifier(discr)
-        if discr>=0:
+        if discr>0:
+            sqr = square_root_simplifier(discr)
             num = math.sqrt(discr)
-            if isinstance(num, int)==True:
-                await ctx.send(f"{ctx.author.display_name}, equation: x = {-b+num/2*a} ,  {-b-num/2*a}")
-            elif isinstance(num, int)==False:
-                await ctx.send(f"{ctx.author.display_name}, equation: x = -({b} ± √{discr})/{2*a} ")
+            if num%1==0:
+                await ctx.send (f"{ctx.author.display_name}, equation: x = {-b+num/2*a} ,  {-b-num/2*a}")
+            elif num%1!=0:
+                if sqr[0]==1:
+                    await ctx.send (f"{ctx.author.display_name}, equation: x = ({-b} ± √{sqr[1]})/{2*a} ")
+                elif sqr[0]!=1:
+                    await ctx.send (f"{ctx.author.display_name}, equation: x = ({-b} ± {sqr[0]}√{sqr[1]})/{2*a} ")
+        elif discr==0:
+            await ctx.send (f"{ctx.author.display_name}, equation: x = {-b/2*a} ")
         else:
-            if sqr[0]==1:
-                await ctx.send(f"{ctx.author.display_name}, equation: x = ({-b} ± i√{discr})/{2*a}")
-            else:
-                await ctx.send(f"{ctx.author.display_name}, equation: x = ({-b} ± {sqr[0]}i√{sqr[1]})/{2*a}")
+            discr=0-discr
+            sqr = square_root_simplifier(discr)
+            num = math.sqrt(discr)
+            if num%1==0:
+                await ctx.send (f"{ctx.author.display_name}, equation: x = ({-b} ± i{int(num)})/{2*a}")
+            elif num%1!=0:
+                if sqr[0]==1:
+                    await ctx.send (f"{ctx.author.display_name}, equation: x = ({-b} ± i√{discr})/{2*a}")
+                elif sqr[0]!=1:
+                    await ctx.send (f"{ctx.author.display_name}, equation: x = ({-b} ± {sqr[0]}i√{sqr[1]})/{2*a}")
     elif a==0 & b==0:
-        await ctx.send(f"{ctx.author.display_name}, equation: There is no x to solve for")
+        await ctx.send( f"{ctx.author.display_name}, equation: There is no x to solve for")
 
 @bot.command(name = "sqrt_simplifier", help = "Simplifies square roots, takes in radicand as argument.")
-async def sqrt_simplifier(ctx, radicand):
+async def sqrt_simplifier(ctx, radicand:int):
     ans = square_root_simplifier(radicand)
     if radicand>0:
-        await ctx.send(f"{ctx.author.display_name} sqrt simplifier: {ans[0]}√{ans[1]}")
+        if ans[1]==1:
+            await ctx.send(f"{ctx.author.display_name} sqrt simplifier: {ans[0]}")
+        else:
+            await ctx.send(f"{ctx.author.display_name} sqrt simplifier: {ans[0]}√{ans[1]}")
     elif radicand == 0:
         await ctx.send("0")
-    else:
-        await ctx.send(f"{ctx.author.display_name}, sqrt simplifier: {ans[0]}i√{ans[1]}")
+    elif radicand<0:
+        ans = square_root_simplifier(-radicand)
+        if ans[1]==1:
+            await ctx.send(f"{ctx.author.display_name}, sqrt simplifier: {ans[0]}i")
+        else:
+            await ctx.send(f"{ctx.author.display_name}, sqrt simplifier: {ans[0]}i√{-ans[1]}")
         
 
 
 @bot.command(name = "plot", help = "")
-async def plot(ctx, *args, x):
+async def plot(ctx, *args:int, x:int):
     args=[*args]
     answr=0
     degree = len(args)-1
@@ -82,40 +100,45 @@ async def backstory(ctx):
     await ctx.send("Long ago, in the very center of the Earth, a single rock of magma and hot gases combined with an unseen power, creating an ominpotent being of mass-destruction. Feared by all, the beast was contained in an ancient form of hydroflask by the first VSCO girls. They worshipped the being from the hydroflask for centuries, until a tourist accidentally opened the bottle. The beast arose from it's prison of plastic, taking form. The beast began it's destructive course. After decades of hiding, a single human was able to tame the beast. Over time, it developed a body to control, that of a small lemur. The body allowed the beast to communicate with humans. The being was further tamed to solve mathematical equations and now spends it's days solving the problems of any humans that give sacrifices. He likes discord and spends most of his time there, where he set up a service for helping humans with math. The creature was Mort.")
 
 @bot.command(name="Power", help="Input a number and the exponent (in that order) and Mort returns the answer")
-async def power(ctx, a, y):
+async def power(ctx, a:int, y:int):
     await ctx.send(f"{ctx.author.display_name}, power: {a**y}")
 
 @bot.command(name = "linear_systems", help="Solves linear systems of equations in the form of y = ax + b and y = cx + d. Enter the a, b, c, and d of the equations.")
-async def linear_systems(ctx, a, b, c, d):    
+async def linear_systems(ctx, a:int, b:int, c:int, d:int):    
     x = (d-b)/(a-c)    
     y = (a*x)+b    
     await ctx.send(f"Your x value is equal to {x} and your y value is equal to {y}")
     
-@bot.command(name="add_subtract", help = "takes values that you want to add and subtract. eg. putting in 12, -10, 1 would give you 3.")
-async def add_and_subtract(ctx, *args):
-    await ctx.send(sum(*args))
+@bot.command("add_sub", help = "takes values that you want to add and subtract. eg. putting in 12, -10, 1 would give you 3.")
+async def add_and_subtract(ctx, *values):
+    ans=0
+    for i in values:
+        ans= ans + i
+    await ctx.send(f"{ctx.authour.display_name}, add_sub: {ans}")
+
+
     
 @bot.command(name = "multiply", help= "multiplies two values")
-async def multiply(ctx, x,y):
+async def multiply(ctx,x:int,y:int):
     await ctx.send(x*y)
 
 @bot.command(name = "divide", help = "divides two values")
-async def divide(ctx, x,y):
+async def divide(ctx, x:int,y:int):
     await ctx.send(x/y)
 
 
 
 def perfect_squares_generator(n):
     s = 2
-    while n>s**2:
+    while n>=s**2:
         yield s**2
         s+=1
 def square_root_simplifier(radicand):
     coefficient = 1
-    n = perfect_squares_generator(radicand/2)
+    n = perfect_squares_generator(radicand)
     try:
         s = next(n)
-        while s<radicand/2:
+        while s<=radicand:
             while radicand%s==0:
                 radicand/=s
                 coefficient*=math.sqrt(s)
@@ -124,5 +147,5 @@ def square_root_simplifier(radicand):
         pass
     return [int(coefficient), int(radicand)]
 
-bot.run(TOKEN)
 
+bot.run(TOKEN)
